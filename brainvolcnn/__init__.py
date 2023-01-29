74de80b2c112591b724e56ae1f29033f7510ceeb
eminSerin <emin.serin@charite.de>
2023-01-29 10:59:20 +0100

    - Fix modules.

---
+++ brainvolcnn/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "train",
+    "predict",
+    "utils",
+    "models",
+    "data",
+    "losses",
+    "metrics",
+    "callbacks",
+]
+
+from .predict import predict
+from .train import train
---
+++ brainvolcnn/models/__init__.py
@@ -0,0 +1,4 @@
+from res_unet import ResUNet
+from tavor import Tavor
+from unet import UNet
+from vnet import VNet
